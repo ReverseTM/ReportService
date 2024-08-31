c719ef8b6568ee38f2ba8c345144f3d00eb78a05
Reverse <SteamGamesMy@mail.ru>
2024-08-31 23:32:14 +0300

    added db context

---
+++ ReportService.Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportService.Data.Entities;
+
+namespace ReportService.Data.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Report> Reports { get; set; }
+    
+    public DbSet<Author> Authors { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
