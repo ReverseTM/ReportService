9f0fc9be1be3a393f4f41bc8bfee5cce5ee849d9
Reverse <SteamGamesMy@mail.ru>
2024-09-01 00:21:06 +0300

    added ReportRepository to ReportService

---
+++ ReportService.Api/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ReportService.Api.Services;
+using ReportService.Data.DbContexts;
+using ReportService.Data.Entities;
+using ReportService.Data.Interfaces;
+using ReportService.Data.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+    }
+);
+
+builder.Services.AddScoped<IRepository<Report>, ReportRepository>();
+
+var app = builder.Build();
+
+app.MapGrpcService<ReportServiceImpl>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
--- ReportService.Api/Services/ReportServiceImpl.cs
+++ ReportService.Api/Services/ReportServiceImpl.cs
@@ -1,14 +1,17 @@
 using Grpc.Core;
+using ReportService.Data.Entities;
+using ReportService.Data.Interfaces;
 
 namespace ReportService.Api.Services;
 
 public class ReportServiceImpl : ReportSubmitter.ReportSubmitterBase
 {
     private readonly ILogger<ReportServiceImpl> _logger;
-
-    public ReportServiceImpl(ILogger<ReportServiceImpl> logger)
+    private readonly IRepository<Report> _reportRepository;
+    public ReportServiceImpl(ILogger<ReportServiceImpl> logger, IRepository<Report> reportRepository)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _reportRepository = reportRepository ?? throw new ArgumentNullException(nameof(reportRepository));
     }
 
     public override Task<ReportResponse> SubmitReport(ReportRequest request, ServerCallContext context)
