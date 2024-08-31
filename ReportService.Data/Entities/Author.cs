565bd080e6f721cd36acb889213be6a3d879ee39
Reverse <SteamGamesMy@mail.ru>
2024-08-31 23:27:21 +0300

    created entities for EF

---
+++ ReportService.Data/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace ReportService.Data.Entities;
+
+public class Author
+{
+    public long Id { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public ICollection<Report> Reports { get; set; }
+}
---
+++ ReportService.Data/Entities/Report.cs
@@ -0,0 +1,16 @@
+﻿namespace ReportService.Data.Entities;
+
+public class Report
+{
+    public long Id { get; set; }
+    
+    public long AuthorId { get; set; }
+    
+    public DateTime RequestTime { get; set; }
+    
+    public string WordFileUrl { get; set; }
+    
+    public string ReferencedFiles { get; set; }
+    
+    public Author Author { get; set; }
+}
