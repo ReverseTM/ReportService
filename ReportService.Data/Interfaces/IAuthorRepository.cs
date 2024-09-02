8f6d4ac0d6fa3a550e6e1b689acd99b3f80261e9
Reverse <SteamGamesMy@mail.ru>
2024-09-02 14:07:32 +0300

    added an interface and implementation for AuthorRepository

---
+++ ReportService.Data/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportService.Data.Interfaces;
+
+public interface IAuthorRepository<T> where T : class
+{
+    Task<T?> GetById(long id);
+    Task<T?> GetByEmail(string email);
+    Task<IEnumerable<T>> GetAll();
+    Task Add(T entity);
+    Task Update(T entity);
+    Task Delete(long id);
+}
--- ReportService.Data/Repositories/AuthorRepository.cs
+++ ReportService.Data/Repositories/AuthorRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using ReportService.Data.DbContexts;
 using ReportService.Data.Entities;
 using ReportService.Data.Interfaces;
 
 namespace ReportService.Data.Repositories;
 
-public class AuthorRepository : IRepository<Author>
+public class AuthorRepository : IAuthorRepository<AuthorEntity>
 {
     private readonly ApplicationDbContext _context;
 
@@ -14,27 +15,36 @@ public class AuthorRepository : IRepository<Author>
         _context = context ?? throw new ArgumentNullException(nameof(context));
     }
     
-    public async Task<Author?> GetById(long id)
+    public async Task<AuthorEntity?> GetById(long id)
     {
         return await _context.Authors
             .AsNoTracking()
             .FirstOrDefaultAsync(a => a.Id == id);
     }
 
-    public async Task<IEnumerable<Author>> GetAll()
+    public async Task<AuthorEntity?> GetByEmail(string email)
+    {
+        
+        return await _context.Authors
+            .AsNoTracking()
+            .SingleOrDefaultAsync(a => a.Email == email);
+        
+    }
+    
+    public async Task<IEnumerable<AuthorEntity>> GetAll()
     {
         return await _context.Authors
             .AsNoTracking()
             .ToListAsync();
     }
 
-    public async Task Add(Author entity)
+    public async Task Add(AuthorEntity entity)
     {
         await _context.Authors.AddAsync(entity);
         await _context.SaveChangesAsync();
     }
 
-    public async Task Update(Author entity)
+    public async Task Update(AuthorEntity entity)
     {
         _context.Authors.Update(entity);
         await _context.SaveChangesAsync();
