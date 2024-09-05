580ce7519e8f668dc460c2f938a13edfc8a61ced
Reverse <SteamGamesMy@mail.ru>
2024-09-05 14:40:40 +0300

    renamed author and report entities

---
+++ ReportService.Data/Entities/AuthorEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace ReportService.Data.Entities;
+
+/// <summary>
+/// Represents an author entity with properties for identifying and describing an author.
+/// </summary>
+public sealed class AuthorEntity
+{
+    public long Id { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public ICollection<ReportEntity> Reports { get; set; }
+}
---
+++ ReportService.Data/Entities/ReportEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace ReportService.Data.Entities;
+
+/// <summary>
+/// Represents a report entity with details about the report, including its associated author and metadata.
+/// </summary>
+public sealed class ReportEntity
+{
+    public long Id { get; set; }
+    
+    public long AuthorId { get; set; }
+    
+    public DateTime RequestTime { get; set; }
+    
+    public string WordFileUrl { get; set; }
+    
+    public string ReferencedFiles { get; set; }
+    
+    public AuthorEntity Author { get; set; }
+}
